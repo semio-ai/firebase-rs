3d231893157a2d4c72cec96486293a278adf1de5
Victor Paléologue <victor.paleologue@semio.ai>
2024-11-15 16:50:58 +0100

    Accept HTTP connections to localhost
    This is useful to allow connections to Firebase emulators run locally.

--- src/sse.rs
+++ src/sse.rs
@@ -1,17 +1,33 @@
 use eventsource_client::*;
 use futures_util::StreamExt;
+use url::Url;
+
+enum UrlScheme {
+    Https,
+    Http,
+}
 
 pub struct ServerEvents {
-    client: ClientBuilder,
+    client_builder: ClientBuilder,
+    scheme: UrlScheme,
 }
 
 impl ServerEvents {
     pub fn new(url: &str) -> Option<Self> {
-        let client = ClientBuilder::for_url(url);
+        let client_builder_result = ClientBuilder::for_url(url);
+        let scheme = match Url::parse(url) {
+            Ok(url) => match url.scheme() {
+                "https" => UrlScheme::Https,
+                "http" => UrlScheme::Http,
+                _ => return None,
+            },
+            Err(_) => return None,
+        };
 
-        match client {
-            Ok(stream_connection) => Some(ServerEvents {
-                client: stream_connection,
+        match client_builder_result {
+            Ok(client_builder) => Some(ServerEvents {
+                client_builder,
+                scheme,
             }),
             Err(_) => None,
         }
@@ -34,32 +50,34 @@ impl ServerEvents {
             .await
     }
 
+    /**
+     * Create a stream of server-sent events.
+     */
     pub fn stream(
         self,
         keep_alive_friendly: bool,
     ) -> std::pin::Pin<Box<dyn futures_util::Stream<Item = Result<(String, Option<String>)>> + Send>>
     {
-        return Box::pin(
-            self.client
-                .build()
-                .stream()
-                .filter_map(move |event| async move {
-                    match event {
-                        Ok(SSE::Event(ev)) => {
-                            if ev.event_type == "keep-alive" && !keep_alive_friendly {
-                                return None;
-                            }
-
-                            if ev.data == "null" {
-                                return Some(Ok((ev.event_type, None)));
-                            }
-
-                            return Some(Ok((ev.event_type, Some(ev.data))));
-                        }
-                        Ok(SSE::Comment(_)) => return None,
-                        Err(x) => Some(Err(x)),
+        let client: Box<dyn Client> = match self.scheme {
+            UrlScheme::Https => Box::new(self.client_builder.build()),
+            UrlScheme::Http => Box::new(self.client_builder.build_http()),
+        };
+        return Box::pin(client.stream().filter_map(move |event| async move {
+            match event {
+                Ok(SSE::Event(ev)) => {
+                    if ev.event_type == "keep-alive" && !keep_alive_friendly {
+                        return None;
                     }
-                }),
-        );
+
+                    if ev.data == "null" {
+                        return Some(Ok((ev.event_type, None)));
+                    }
+
+                    return Some(Ok((ev.event_type, Some(ev.data))));
+                }
+                Ok(SSE::Comment(_)) => return None,
+                Err(x) => Some(Err(x)),
+            }
+        }));
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,20 @@
+use std::net::{Ipv4Addr, Ipv6Addr};
+
 use crate::errors::UrlParseResult;
 use crate::UrlParseError;
 use url::Url;
+
+fn is_locahost(uri: &Url) -> bool {
+    if let Some(host) = uri.host() {
+        match host {
+            url::Host::Domain(domain) => domain == "localhost",
+            url::Host::Ipv4(ip) => ip == Ipv4Addr::LOCALHOST,
+            url::Host::Ipv6(ip) => ip == Ipv6Addr::LOCALHOST,
+        }
+    } else {
+        false
+    }
+}
 
 pub fn check_uri(uri: &str) -> UrlParseResult<Url> {
     let uri = Url::parse(uri);
@@ -10,7 +24,7 @@ pub fn check_uri(uri: &str) -> UrlParseResult<Url> {
         Err(err) => return Err(UrlParseError::Parser(err)),
     };
 
-    if uri.scheme() != "https" {
+    if uri.scheme() != "https" && !is_locahost(&uri) {
         return Err(UrlParseError::NotHttps);
     }
 
